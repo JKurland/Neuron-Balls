9b5f1fa384274898228025582cd2b4254d975fcf
JKurland <joshua.kurland@outlook.com>
2017-06-07 16:00:25 +0100

    final wednesday

--- Assets/Project Assets/Scripts/AudioController.cs
+++ Assets/Project Assets/Scripts/AudioController.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class AudioController : MonoBehaviour {
 	
 	AudioSource shot;
-	AudioSource thud;
+	AudioSource[] playerThuds;
+	AudioSource[] groundThuds;
+
+	int groundCollisions = 0;
+
 	// Use this for initialization
 	void Start () {
 		AudioSource[] audios = GetComponents<AudioSource> ();
 		shot = audios [0];
-		thud = audios [1];
+		playerThuds = slice (audios, 1, 3);
+		groundThuds = slice (audios, 3, -1);
+		Random.InitState (Time.frameCount);
 	}
 	
 	// Update is called once per frame
@@ -19,10 +25,38 @@ public class AudioController : MonoBehaviour {
 	}
 
 	void OnCollisionEnter(Collision other){
-		Debug.Log (other.gameObject.name);
+		double pitchShift = 0.2*(Random.value - 0.5) + 1f;
+
 		if (other.gameObject.name == "Floor") {
-			thud.Play ();
+			
+			int skip = Random.Range (0, 2);
+
+			groundThuds [groundCollisions + skip].pitch = (float)pitchShift;
+			groundThuds[groundCollisions + skip].Play ();
+			groundCollisions += 1;
+			if ( groundCollisions >= groundThuds.Length - 1){
+				groundCollisions = groundThuds.Length - 2;
+			}
 		}
 
+		if (other.gameObject.name == "ColliderR" || other.gameObject.name == "ColliderL") {
+			int r = Random.Range (0, playerThuds.Length);
+			playerThuds [r].Play ();
+		}
+
+	}
+		
+	public static T[] slice<T>(T[] l, int from, int to)
+	{
+		if (to == -1) {
+			to = l.Length;
+		}
+
+		T[] r = new T[to - from];
+		for (int i = from; i < to; i++)
+		{
+			r[i-from]=l[i];
+		}
+		return r;
 	}
 }
